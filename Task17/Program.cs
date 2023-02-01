b22628e76733b2a8e690b1b407e3e7a06205f017
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-02-01 19:01:05 +0500

    Добавлена проверка

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -4,9 +4,19 @@ double x = Convert.ToDouble(Console.ReadLine());
 
 Console.Write("Введите координату Y: ");
 double y = Convert.ToDouble(Console.ReadLine());
+
+while (x == 0 || y == 0)
+{
+Console.WriteLine("Вы ошиблись! Введите координаты заново!");
+Console.Write("Введите координату X: ");
+x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+y = Convert.ToDouble(Console.ReadLine());
+}
+
 if (x > 0 && y > 0)
    Console.WriteLine("I");
-   
+
 else if (x < 0 && y > 0)
    Console.WriteLine("II");
 
