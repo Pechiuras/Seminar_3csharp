7d5a0b76e4e70580dd08ef27a443b2f35ba8ec85
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-02-01 18:43:49 +0500

    Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите координату Х: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату Y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+if (x > 0 && y > 0)
+   Console.WriteLine("I");
+   
+else if (x < 0 && y > 0)
+   Console.WriteLine("II");
+
+else if (x < 0 && y < 0)
+   Console.WriteLine("III");
+
+else
+   Console.WriteLine("IV");
