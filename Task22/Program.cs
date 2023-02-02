6f82f5dede1775dec81d7e9d5d4f624e12d1ed3c
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-02-02 16:54:37 +0500

    Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,8 @@
+﻿// Task 22 Вывод квадратов чисел от 1 до N
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+    Console.Write($"{Math.Pow(i, 2)}");
+
