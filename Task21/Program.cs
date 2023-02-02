f7566421bca36a1dd0b0908e23936c7e19a28e31
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-02-02 16:53:27 +0500

    Task21

---
+++ Task21/Program.cs
@@ -0,0 +1,16 @@
+﻿//Task21
+Console.Clear();
+Console.Write("Введите Х1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите Y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите X2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите Y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+double l = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)), 2);
+Console.WriteLine(l);
