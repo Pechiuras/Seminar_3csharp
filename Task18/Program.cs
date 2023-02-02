c136b6caa196a9bbd0aea4b374744da2165f5b2f
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-02-02 16:52:21 +0500

    Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите четверть ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (n < 1 || n > 4)
+{
+Console.WriteLine("Вы ошиблись! \nВведите данные заново: ");
+n = Convert.ToInt32(Console.ReadLine());
+}
+
+if (n == 1)
+   Console.WriteLine("x > 0, y > 0");
+
+else if (n == 2)
+   Console.WriteLine("x < 0, y > 0");
+
+else if (n == 3)
+   Console.WriteLine("x < 0,  y < 0");
+
+else if(n == 4)
+   Console.WriteLine("x > 0,  y < 0");
